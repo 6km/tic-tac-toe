dec8d7226f4988e79ade36b83d20d945009d538c
Mohammed <gsft22221@gmail.com>
2024-03-09 00:08:18 +0200

    Update Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,12 @@ import React from "react"
 import Logo from "../assets/logo.svg?react"
 
 function Header() {
-	return <div>
+	return <div className="header">
 		<Logo height={23} />
+		<p className="author">
+			{"by "}
+			<a href="https://github.com/6km/tic-tac-toe" target="_blank">@6km</a>
+		</p>
 	</div>
 }
 
